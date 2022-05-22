219ecb5787d304bacb2466a5b49bdd24859ba521
Kaiser Kamruzzaman <38045495+KaiserKamruzzaman@users.noreply.github.com>
2022-05-23 00:47:48 +0200

    event bus added and integrate with post and comment

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const { default: axios } = require("axios");
 const cors = require("cors");
 
 const app = express();
@@ -12,7 +13,7 @@ const commentsByPostId = {};
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
@@ -21,7 +22,19 @@ app.post("/posts/:id/comments", (req, res) => {
     content,
   });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { id: commentId, content },
+    postId: req.params.id,
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Receive Comment Event..", req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
